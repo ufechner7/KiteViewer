9d4c6796ff2e5ee9735f9ddfb7dde2fd5b52039c
Uwe Fechner <fechner@aenarete.eu>
2021-01-26 20:41:37 +0100

    ensure compatibility with windows

--- kiteviewer.sh
+++ kiteviewer.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-source ~/.bashrc
+if test -f "~/.bashrc"; then
+    source ~/.bashrc
+fi
 
 echo "Lauching KiteViewer..."
-julia --optimize=1 --project -e "include(\"./src/KiteViewer.jl\");main(true)"
+julia6 --optimize=1 --project -e "include(\"./src/KiteViewer.jl\");main(true)"
