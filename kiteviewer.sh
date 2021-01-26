dea8e548f15ef5465958d3d96fe7cdf031606f61
Uwe Fechner <fechner@aenarete.eu>
2021-01-26 02:09:48 +0100

    Now symlink safe.

--- kiteviewer.sh
+++ kiteviewer.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+source ~/.bashrc
+
 echo "Lauching KiteViewer..."
-julia6 --optimize=1 --project -e "include(\"./src/KiteViewer.jl\");main(true)"
+julia --optimize=1 --project -e "include(\"./src/KiteViewer.jl\");main(true)"
