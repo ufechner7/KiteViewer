84fc2c98f4df30567aef5369b207ae2cdc5b53f4
Uwe Fechner <fechner@aenarete.eu>
2021-01-30 16:55:03 +0100

    Minimal.jl now self contained

--- src/Minimal.jl
+++ src/Minimal.jl
@@ -20,13 +20,25 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. =#
 
-using GeometryBasics, GLMakie, FileIO
+using GeometryBasics, GLMakie, StaticArrays
 
-using Revise
-includet("./Utils.jl")
-using .Utils
-
+const MyFloat = Float32
 const SCALE = 1.2
+const SEGMENTS = 7                    # number of tether segments
+
+struct SysState
+    X::MVector{SEGMENTS+1, MyFloat}   # vector of particle positions in x
+    Y::MVector{SEGMENTS+1, MyFloat}   # vector of particle positions in y
+    Z::MVector{SEGMENTS+1, MyFloat}   # vector of particle positions in z
+end 
+
+function demo_state(rel_height=1.0)
+    a = 10
+    X = range(0, stop=10, length=SEGMENTS+1)
+    Y = zeros(length(X)) 
+    Z = (a .* cosh.(X./a) .- a) * rel_height 
+    return SysState(X, Y, Z)
+end
 
 function create_coordinate_system(scene, points = 10, length = 10)
     # create origin
