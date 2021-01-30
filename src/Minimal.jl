caaadd902f340c7384c22931b9c9565b394b9165
Uwe Fechner <fechner@aenarete.eu>
2021-01-30 17:06:49 +0100

    Further simplifications

--- src/Minimal.jl
+++ src/Minimal.jl
@@ -1,44 +1,7 @@
-#= MIT License
+using GeometryBasics, GLMakie
 
-Copyright (c) 2020 Uwe Fechner
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE. =#
-
-using GeometryBasics, GLMakie, StaticArrays
-
-const MyFloat = Float32
 const SCALE = 1.2
 const SEGMENTS = 7                    # number of tether segments
-
-struct SysState
-    X::MVector{SEGMENTS+1, MyFloat}   # vector of particle positions in x
-    Y::MVector{SEGMENTS+1, MyFloat}   # vector of particle positions in y
-    Z::MVector{SEGMENTS+1, MyFloat}   # vector of particle positions in z
-end 
-
-function demo_state(rel_height=1.0)
-    a = 10
-    X = range(0, stop=10, length=SEGMENTS+1)
-    Y = zeros(length(X)) 
-    Z = (a .* cosh.(X./a) .- a) * rel_height 
-    return SysState(X, Y, Z)
-end
 
 function create_coordinate_system(scene, points = 10, length = 10)
     # create origin
@@ -67,18 +30,17 @@ function main(gl_wait=false)
     layout[1, 1] = scene3D
     layout[2, 1] = buttongrid = GridLayout(tellwidth = false)
 
-    btn_RESET = Button(scene, label = "RESET")
-    btn_ZOOM_in = Button(scene, label = "Zoom +")
-    btn_ZOOM_out = Button(scene, label = "Zoom -")
-
-    buttongrid[1, 1:3] = [btn_RESET, btn_ZOOM_in, btn_ZOOM_out]
+    buttongrid[1, 1:1] = [Button(scene, label = "RESET")]
 
     for i = 0:4
         # calculate a vector of 3D coordinates
-        state = demo_state(i/5.0)
+        X = range(0, stop=10, length=SEGMENTS+1)
+        Y = zeros(length(X)) 
+        Z = (10 .* cosh.(X./10) .- 10) * i/5.0 
+
         # loop over the particles of the main tether and render them as spheres
-        for i in range(1, length=length(state.X))
-            mesh!(scene3D, Sphere(Point3f0(state.X[i], state.Y[i], state.Z[i]), 0.07 * SCALE), color=:yellow)
+        for i in range(1, length=length(X))
+            mesh!(scene3D, Sphere(Point3f0(X[i], Y[i], Z[i]), 0.07 * SCALE), color=:yellow)
         end
         display(scene)
         
