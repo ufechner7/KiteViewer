a3fb30d1bbd6b0467b69272b0140fe0a8c2529e1
Uwe Fechner <fechner@aenarete.eu>
2021-02-24 19:56:36 +0100

    Added results.

--- test/time_bug.jl
+++ test/time_bug.jl
@@ -18,3 +18,14 @@ tock()
 # 4.336227544910179
 
 # @time reports 0.67 seconds, while the real execution time incl. compilation is more like 4.3 seconds
+
+# On Julia 1.5.3:
+# julia> include("test/time_bug.jl")
+# [ Info:  started timer at: 2021-02-21T10:59:55.463
+#   0.811249 seconds (1.94 M allocations: 103.003 MiB, 2.59% gc time)
+# [ Info:          2.161581335s: 2 seconds, 161 milliseconds
+
+# julia> 2.161/0.811
+# 2.664611590628853
+
+# @time reports 0.81 seconds, while the real execution time incl. compilation is more like 2.16 seconds
