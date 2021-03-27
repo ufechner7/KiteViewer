8d19234c66ec3b145cc9ab4a7d7f07e436f72eff
Uwe Fechner <fechner@aenarete.eu>
2021-03-27 13:32:06 +0100

    Update packages before creating sys image.

--- create_sys_image.sh
+++ create_sys_image.sh
@@ -2,4 +2,5 @@
 if test -f "MakieSys.so"; then
     cp MakieSys.so MakieSys.so.bak
 fi
+julia --project  -e "include(\"./test/update_packages.jl\");"
 julia --project  -e "include(\"./test/create_sys_image.jl\");"
