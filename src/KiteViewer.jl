4a0ac4eb7c0e3b8aa4b177920215a68da63ba386
Uwe Fechner <uwe.fechner@nl.abb.com>
2021-04-25 18:41:09 +0200

    Removed obsolete package Makie. Just use GLMakie!

--- src/KiteViewer.jl
+++ src/KiteViewer.jl
@@ -20,9 +20,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. =#
 
-using GeometryBasics, Rotations, Makie
+using GeometryBasics, Rotations, GLMakie, FileIO
 AbstractPlotting.__init__()
-using GLMakie, FileIO
 
 using Revise
 includet("./Utils.jl")
