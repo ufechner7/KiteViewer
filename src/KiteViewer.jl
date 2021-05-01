3cca42566ff9f2778cb44115578b4a014453ff82
Uwe Fechner <uwe.fechner@nl.abb.com>
2021-05-02 01:26:40 +0200

    Text is drawn only once now, much better GUI performance

--- src/KiteViewer.jl
+++ src/KiteViewer.jl
@@ -36,6 +36,7 @@ const FLYING    = [false]
 const PLAYING    = [false]
 const GUI_ACTIVE = [false]
 const running = Node(false)
+const textnode = Node("")
 
 const points          = Vector{Point3f0}(undef, se().segments+1)
 const quat            = Node(Quaternionf0(0,0,0,1))                        # orientation of the kite
@@ -46,7 +47,6 @@ const markersizes     = Node([Point3f0(1,1,1) for x in 1:se().segments])   # inc
 const rotations       = Node([Point3f0(1,0,0) for x in 1:se().segments])   # unit vectors corresponding with
                                                                            #   the orientation of the segments 
 const energy = [0.0]                                                                           
-const text = [undef, 1]
 
 function create_coordinate_system(scene, points = 10, max_x = 15.0)
     # create origin
@@ -91,16 +91,25 @@ function create_coordinate_system(scene, points = 10, max_x = 15.0)
     end 
 end
 
-# draw the kite power system, consisting of the tether and the kite
+# draw the kite power system, consisting of the tether, the kite and the state (text and numbers)
 function init_system(scene)
     sphere = Sphere(Point3f0(0, 0, 0), Float32(0.07 * SCALE))
     meshscatter!(scene, part_positions, marker=sphere, markersize=1.0, color=:yellow)
     cyl = Cylinder(Point3f0(0,0,-0.5), Point3f0(0,0,0.5), Float32(0.035 * SCALE))        
     meshscatter!(scene, positions, marker=cyl, rotations=rotations, markersize=markersizes, color=:yellow)
     meshscatter!(scene, kite_pos, marker=KITE, markersize = 0.25, rotations=quat, color=:blue)
+    if Sys.islinux()
+        font="/usr/share/fonts/truetype/ttf-bitstream-vera/VeraMono.ttf"
+    else
+        font="Courier New"
+    end
+    if se().fixed_font != ""
+        font=se().fixed_font
+    end
+    text!(scene, textnode, position = Point3f0(-5.2, 3.5, -1), textsize = 16, font=font, align = (:left, :top))
 end
 
-# update the kite power system, consisting of the tether and the kite
+# update the kite power system, consisting of the tether, the kite and the state (text and numbers)
 function update_system(scene, state, step=0)
 
     # move the particles to the correct position
@@ -122,7 +131,7 @@ function update_system(scene, state, step=0)
     # print state values
     power = state.force * state.v_reelout
     energy[1] += (power / se().sample_freq * 2)
-    if mod(step, 4) == 0
+    if mod(step, 2) == 0
         height = points[end][3]/se().zoom
         msg = "time:      $(@sprintf("%7.2f", state.time)) s\n" *
             "height:    $(@sprintf("%7.2f", height)) m\n" *
@@ -130,19 +139,7 @@ function update_system(scene, state, step=0)
             "azimuth:   $(@sprintf("%7.2f", state.azimuth/pi*180.0)) °\n" *
             "v_reelout: $(@sprintf("%7.2f", state.v_reelout)) m/s   " * "p_mech:  $(@sprintf("%8.2f", state.force*state.v_reelout)) W\n" *
             "force:     $(@sprintf("%7.2f", state.force    )) N     " * "energy:  $(@sprintf("%8.2f", energy[1]/3600)) Wh\n"
-
-        if typeof(text[1]) == AbstractPlotting.Text{Tuple{String}}
-            delete!(scene.scene, text[1])
-        end
-        if Sys.islinux()
-            font="/usr/share/fonts/truetype/ttf-bitstream-vera/VeraMono.ttf"
-        else
-            font="Courier New"
-        end
-        if se().fixed_font != ""
-            font=se().fixed_font
-        end
-        text[1] = text!(scene, msg , position = Point3f0(-5.2, 3.5, -1), textsize = 16, font=font, align = (:left, :top))
+        textnode[] = msg   
     end
 end
 
@@ -309,7 +306,6 @@ function main(gl_wait=true)
     FLYING[1] = false
     GUI_ACTIVE[1] = false
     running[] = false
-    text[1] = undef
     energy[1] = 0.0
     return nothing
 end
