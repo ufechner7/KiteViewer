6145fd6e5e776eb2a07d3e9c7ce960c7d23356f8
Uwe Fechner <uwe.fechner@nl.abb.com>
2021-05-06 03:37:52 +0200

    Improved script for creating a compiled image.

--- create_sys_image.sh
+++ create_sys_image.sh
@@ -2,5 +2,6 @@
 if test -f "MakieSys.so"; then
     cp MakieSys.so MakieSys.so.bak
 fi
-julia --project  -e "include(\"./test/update_packages.jl\");"
-julia --project  -e "include(\"./test/create_sys_image.jl\");"
+julia --project -e "include(\"./test/update_packages.jl\");"
+julia --project -e "using Pkg; Pkg.precompile()"
+julia --project -e "include(\"./test/create_sys_image.jl\");"
