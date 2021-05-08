725e2390946d10e4c552c8f7f35a47e9ae4f52e5
Uwe Fechner <fechner@aenarete.eu>
2021-05-08 15:28:29 +0200

    Delete Manifest.toml before updating

--- create_sys_image.sh
+++ create_sys_image.sh
@@ -2,6 +2,7 @@
 if test -f "MakieSys.so"; then
     cp MakieSys.so MakieSys.so.bak
 fi
+rm -f Manifest.toml
 julia --project -e "include(\"./test/update_packages.jl\");"
 julia --project -e "using Pkg; Pkg.precompile()"
 julia --project -e "include(\"./test/create_sys_image.jl\");"
