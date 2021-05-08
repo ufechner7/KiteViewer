edabe62216f9e4f6d3256c88a7727e59d969a3c5
Uwe Fechner <uwe.fechner@nl.abb.com>
2021-05-08 14:21:43 +0200

    Added checks to performance.sh

--- performance.sh
+++ performance.sh
@@ -1,4 +1,15 @@
 #! /bin/bash
 
+if [ $(uname) != "Linux" ] ; then
+    echo "For now, setting the performance is only working on Linux..."
+    exit 1
+fi
+
+# Check root permissions
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
 echo "Setting the cpu governor to performance..."
 for ((i=0;i<$(nproc);i++)); do cpufreq-set -c $i -g performance; done
