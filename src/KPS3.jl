6b6847e4181094808888dec64d0454d2b4133883
Uwe Fechner <fechner@aenarete.eu>
2021-05-10 20:00:49 +0200

    Finished implementation of calc_aero_forces

--- src/KPS3.jl
+++ src/KPS3.jl
@@ -47,8 +47,12 @@ const Vec3    = MVector{3, MyFloat}
 # constants
 const G_EARTH = 9.81                # gravitational acceleration
 const C0 = -0.0032                  # steering offset
+const C2_COR =  0.93
 const CD_TETHER = se().cd_tether    # tether drag coefficient
 const L_BRIDLE = se().l_bridle      # sum of the lengths of the bridle lines [m]
+const REL_SIDE_AREA = 0.5
+const STEERING_COEFFICIENT = 0.6
+const BRIDLE_DRAG = 1.1
 const ALPHA = se().alpha
 
 const K_ds = 1.5 # influence of the depower angle on the steering sensitivity
@@ -56,7 +60,6 @@ const MAX_ALPHA_DEPOWER = 31.0 # was: 44
 
 const ALPHA_CL = [-180.0, -160.0, -90.0, -20.0, -10.0,  -5.0,  0.0, 20.0, 40.0, 90.0, 160.0, 180.0]
 const CL_LIST  = [   0.0,    0.5,   0.0,  0.08, 0.125,  0.15,  0.2,  1.0,  1.0,  0.0,  -0.5,   0.0]
-
 const ALPHA_CD = [-180.0, -170.0, -140.0, -90.0, -20.0, 0.0, 20.0, 90.0, 140.0, 170.0, 180.0]
 const CD_LIST  = [   0.5,    0.5,    0.5,   1.0,   0.2, 0.1,  0.2,  1.0,   0.5,   0.5,   0.5]
 
@@ -77,16 +80,20 @@ mutable struct State
     v_apparent::Vec3
     drag_force::Vec3
     lift_force::Vec3
+    steering_force::Vec3
+    last_force::Vec3
     kite_y::Vec3
-    temp::Vec3
     seg_area::MyFloat   # area of one tether segment
     param_cl::MyFloat
     param_cd::MyFloat
     v_app_norm::MyFloat
+    cor_steering::MyFloat
+    psi::MyFloat
+    beta::MyFloat
 end
 
 function init()
-    state = State(zeros(3), zeros(3), zeros(3), zeros(3), zeros(3), zeros(3), zeros(3), zeros(3), 0.0,  0.0, 0.0, 0.0)
+    state = State(zeros(3), zeros(3), zeros(3), zeros(3), zeros(3), zeros(3), zeros(3), zeros(3), zeros(3), 0.0,  0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
     state.v_wind[1]        = se().v_wind # westwind, downwind direction to the east
     state.v_wind_gnd[1]    = se().v_wind # westwind, downwind direction to the east
     state.v_wind_tether[1] = se().v_wind # wind at half of the height of the kite
@@ -113,24 +120,18 @@ end
 #     paramCD:      drag coefficient (function of power settings)
 #     paramCL:      lift coefficient (function of power settings)
 #     rel_steering: value between -1.0 and +1.0
-function calc_aero_forces(s, pos_kite, v_kite, rho, rel_steering, v_apparent)
-    v_apparent   .= s.v_wind - v_kite
+function calc_aero_forces(s, pos_kite, v_kite, rho, rel_steering)
+    s.v_apparent   .= s.v_wind - v_kite
     s.v_app_norm  = norm(s.v_apparent)
     s.drag_force .= s.v_apparent ./ s.v_app_norm
-    s.kite_y     .= cross(pos_kite, s.drag_force)
-    s.kite_y     .= s.kite_y ./ norm(s.kite_y)
+    s.kite_y     .= normalize(cross(pos_kite, s.drag_force))
     K             = 0.5 * rho * s.v_app_norm^2 * se().area
-    s.temp       .= cross(s.drag_force, s.kite_y)
-    s.temp       .= s.temp ./ norm(s.temp)
-    s.lift_force .= K * s.param_cl .* s.temp
-    # println(s.lift_force)
-#     mul3(K * scalars[ParamCL], vec3[Temp], vec3[Lift_force])
-#     # some additional drag is created while steering
-#     mul2( K * scalars[ParamCD] * BRIDLE_DRAG * (1.0 + 0.6 * abs(rel_steering)), vec3[Drag_force])
-#     scalars[Cor_steering] = C2_COR / scalars[V_app_norm] * math.sin(scalars[Psi]) * math.cos(scalars[Beta])
-#     mul3(- K * REL_SIDE_AREA * STEERING_COEFFICIENT * (rel_steering + scalars[Cor_steering]), vec3[Kite_y], \
-#          vec3[Steering_force])
-#     neg_sum(vec3[Lift_force], vec3[Drag_force], vec3[Steering_force], vec3[Last_force])
+    s.lift_force .= K * s.param_cl .* normalize(cross(s.drag_force, s.kite_y))   
+    # some additional drag is created while steering
+    s.drag_force .*= K * s.param_cd * BRIDLE_DRAG * (1.0 + 0.6 * abs(rel_steering)) 
+    s.cor_steering = C2_COR / s.v_app_norm * sin(s.psi) * cos(s.beta)
+    s.steering_force .= -K * REL_SIDE_AREA * STEERING_COEFFICIENT * (rel_steering + s.cor_steering) .* s.kite_y
+    s.last_force     .= -(s.lift_force + s.drag_force + s.steering_force)
 end
 
 end
--- test/test_kps3.jl
+++ test/test_kps3.jl
@@ -14,25 +14,25 @@ if ! @isdefined state
     const state = init()
 end
 
-@testset "calc_rho        " begin
+@testset "calc_rho             " begin
     @test isapprox(calc_rho(0.0), 1.225, atol=1e-5) 
     @test isapprox(calc_rho(100.0), 1.210756, atol=1e-5) 
 end
 
-@testset "calc_wind_factor" begin
+@testset "calc_wind_factor     " begin
     @test isapprox(calc_wind_factor(6.0),   1.0, atol=1e-5) 
     @test isapprox(calc_wind_factor(10.0),  1.0757037, atol=1e-5) 
     @test isapprox(calc_wind_factor(100.0), 1.494685, atol=1e-5)
 end
 
-@testset "calc_cl         " begin
+@testset "calc_cl              " begin
     @test isapprox(calc_cl(-5.0), 0.150002588978, atol=1e-4) 
     @test isapprox(calc_cl( 0.0), 0.200085035326, atol=1e-4) 
     @test isapprox(calc_cl(10.0), 0.574103590856, atol=1e-4)
     @test isapprox(calc_cl(20.0), 1.0, atol=1e-4)
 end
 
-@testset "test_calc_drag  " begin
+@testset "test_calc_drag       " begin
     v_segment = Vec3(1.0, 2, 3)
     unit_vector = Vec3(2.0, 3.0, 4.0)
     rho = MyFloat(calc_rho(10.0))
@@ -52,9 +52,14 @@ end
     v_kite = Vec3(3.0, 5.0, 2.0)
     rho = MyFloat(calc_rho(10.0))
     rel_steering = 0.1
-    KPS3.calc_aero_forces(state, pos_kite, v_kite, rho, rel_steering, state.v_apparent)
+    state.beta = 0.1
+    state.psi = 0.2
+    KPS3.calc_aero_forces(state, pos_kite, v_kite, rho, rel_steering)
     @test state.v_apparent ≈ [5.0,  -5, -2]
     @test state.kite_y ≈ [ 0.64101597,  0.73258967, -0.22893427]
+    @test state.cor_steering ≈ 0.0250173783309
+    @test state.steering_force ≈ [-15.88482337, -18.15408385, 5.6731512 ]
+    @test state.last_force ≈ [-555.24319976, 544.82004621, 80.49946362]
 end
 
 println("\ncalc_rho:")
@@ -66,6 +71,6 @@ show(@benchmark calc_cl(α) setup=(α=(rand()-0.5) * 360.0))
 println("\ncalc_drag:")
 show(@benchmark calc_cl(calc_drag(state, v_segment, unit_vector, rho, last_tether_drag, v_app_perp, area)) setup=(v_segment = Vec3(1.0, 2, 3); unit_vector = Vec3(2.0, 3.0, 4.0); rho = calc_rho(10.0f0); last_tether_drag = Vec3(0.0, 0.0, 0.0); v_app_perp =  Vec3(0, -3.0, -4.0); area=se().area))
 println("\ncalc_aero_forces:")
-show(@benchmark KPS3.calc_aero_forces(state, pos_kite, v_kite, rho, rel_steering, state.v_apparent) setup=(state.v_apparent .= Vec3(35.1, 52.2, 69.3); pos_kite = Vec3(30.0, 5.0, 100.0);  v_kite = Vec3(3.0, 5.0, 2.0);  rho = MyFloat(calc_rho(10.0));  rel_steering = 0.1))
+show(@benchmark KPS3.calc_aero_forces(state, pos_kite, v_kite, rho, rel_steering) setup=(state.v_apparent .= Vec3(35.1, 52.2, 69.3); pos_kite = Vec3(30.0, 5.0, 100.0);  v_kite = Vec3(3.0, 5.0, 2.0);  rho = MyFloat(calc_rho(10.0));  rel_steering = 0.1))
 
 nothing
