8b28b54ef835324f90221a22407ec8994006d269
Uwe Fechner <fechner@aenarete.eu>
2021-05-13 15:39:12 +0200

    Add plot

--- src/DAE_Example.jl
+++ src/DAE_Example.jl
@@ -1,4 +1,4 @@
-using DifferentialEquations, Sundials
+using DifferentialEquations, Sundials, GLMakie
 # Tutorial example showing how to use an implicit solver 
 # It simulates a falling mass.
 
@@ -19,13 +19,23 @@ acc_0 = [0.0, 0.0, -9.81]   # Initial acceleration
 y0  = vcat(pos_0, vel_0)    # Initial pos, vel
 yd0 = vcat(vel_0, acc_0)    # Initial vel, acc
 
-tspan = (0.0, 10.0)         # time span
+tspan = (0.0, 10.2)         # time span
 
 differential_vars = ones(Bool, 6)
 prob = DAEProblem(res1, yd0, y0, tspan, differential_vars=differential_vars)
 
-y = solve(prob, IDA())
+sol = solve(prob, IDA(), saveat=0.1)
 
-pos_z = y[3, :]
-vel_z = y[6, :]
-nothing
+time = sol.t
+y = sol.u
+
+pos_z = sol[3, :]
+vel_z = sol[6, :]
+
+# plot the result
+f = Figure()
+ax1 = Axis(f[1, 1], yticklabelcolor = :blue, xlabel="time [s]", ylabel = "pos_z [m]")
+ax2 = Axis(f[1, 1], yticklabelcolor = :red, yaxisposition = :right, ylabel = "vel_z [m/s]")
+lines!(ax1, time, pos_z, color=:green)
+lines!(ax2, time, vel_z, color=:red)
+current_figure()
