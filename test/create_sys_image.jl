220fcfbb52e745d8c448b83a9b2109c21ed1eeb4
Uwe Fechner <fechner@aenarete.eu>
2021-05-13 15:39:01 +0200

    Add DifferentialEquations

--- test/create_sys_image.jl
+++ test/create_sys_image.jl
@@ -1,11 +1,11 @@
 @info "Loading packages ..."
-using GLMakie, Arrow, MeshIO, RecursiveArrayTools, Revise, Rotations, StaticArrays, StructArrays, YAML
+using GLMakie, Arrow, MeshIO, RecursiveArrayTools, Revise, Rotations, StaticArrays, StructArrays, YAML, DifferentialEquations, Sundials
 using PackageCompiler
 
 @info "Creating sysimage ..."
 
 PackageCompiler.create_sysimage(
-    [:GLMakie, :Arrow, :MeshIO, :RecursiveArrayTools, :Revise, :Rotations, :StaticArrays, :StructArrays, :YAML];
+    [:GLMakie, :Arrow, :MeshIO, :RecursiveArrayTools, :Revise, :Rotations, :StaticArrays, :StructArrays, :YAML, :DifferentialEquations, :Sundials];
     sysimage_path="MakieSys.so",
     precompile_execution_file=joinpath("test", "test_for_precompile.jl")
 )
--- test/test_for_precompile.jl
+++ test/test_for_precompile.jl
@@ -3,5 +3,8 @@ let
     Utils.test(true)
     main(false)
 end
+let 
+    include("../src/DAE_Example.jl")
+end
 
 @info "Precompile script has completed execution."
