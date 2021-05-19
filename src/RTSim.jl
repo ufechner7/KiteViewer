30476c447751404ee88742e8a2785ea461c6d185
Uwe Fechner <uwe.fechner@nl.abb.com>
2021-05-19 03:12:42 +0200

    More and better solver options

--- src/RTSim.jl
+++ src/RTSim.jl
@@ -10,12 +10,13 @@ my_state = KPS3.get_state()
 clear(my_state)
 y0, yd0 = KPS3.init(my_state)
 
-tspan = (0.0, 0.05)         # time span; fails when changed to (0.0, 0.06)
+tspan = (0.0, 0.063)         # time span; fails when changed to (0.0, 0.064); works with 0.063
 
 differential_vars =  ones(Bool, 36)
 prob = DAEProblem(residual!, yd0, y0, tspan, differential_vars=differential_vars)
+solver = IDA(linear_solver=:Dense, max_order=3, max_convergence_failures=10, max_nonlinear_iters=3, init_all=false) # :BCG :GMRES :Dense :TFQMR :LapackDense
 
-@time sol = solve(prob, IDA(), saveat=0.001, abstol=0.01, reltol=0.001)
+@time sol = solve(prob, solver, saveat=0.001, abstol=0.0000001, reltol=0.001) # inith = 0.002, maxord = 3, abstol=0.0000001
 
 time = sol.t
 println(sol.retcode)
@@ -24,6 +25,7 @@ println(length(y))
 
 pos_x = sol[3*5+1, :]
 pos_z = sol[3*5+3, :]
+nothing
 
 # # plot the result
 # f = Figure()
