b836648effc079b6f056b90b84a192ef3a8ae4d7
Uwe Fechner <fechner@aenarete.eu>
2021-05-22 17:04:41 +0200

    Add function test_nlopt()

--- test/test_nlopt.jl
+++ test/test_nlopt.jl
@@ -1,9 +1,11 @@
+module TestNLopt
+
 using Test, BenchmarkTools, StaticArrays, Revise, LinearAlgebra, SciMLBase, NLopt, GLMakie
 
-if ! @isdefined KPS3
-    includet("../src/KPS3.jl")
-    using .KPS3
-end
+export test_nlopt
+
+include("../src/KPS3.jl")
+using .KPS3
 
 function init_392()
     my_state = KPS3.get_state()
@@ -28,34 +30,48 @@ function test_initial_condition(params::Vector, grad::Vector)
     return norm(res) # z component of force on all particles but the first
 end
 
-lower = SVector{2*SEGMENTS}(-10, -10, -20, -20, -10, -10.0, -5, -5, -5, -5, -5, -5)
-upper = SVector{2*SEGMENTS}( 10,  10,  20,  20,  10,  10.0,  5,  5,  5,  5,  5,  5)
-initial_x = MVector{2*SEGMENTS}(zeros(12))
-init_392()
-# working: :GD_STOGO 1803, :GD_STOGO_RAND 1403, :GN_ESCH 378, :GN_DIRECT_L 122, :GN_DIRECT_L_RAND 116, :GN_ISRES 82.8, :GN_CRS2_LM 60; not working: GN_AGS
-# in 15 min :GN_CRS2_LM: 27;
-# in  1 min :LN_SBPLX 9.96 in 15 min: 1.13
-# in  1 min :LN_BOBYQA 0.31; LN_COBYLA 25; LD_MMA 6046; not working: LD_SLSQP
-# without time limit: 0.0001699365589509266 ROUNDOFF_LIMITED; took 495s, 39628692 numevals or 12.5 us per numeval
-opt = Opt(:LN_BOBYQA, 12) 
-opt.lower_bounds = lower
-opt.upper_bounds = upper
-# opt.xtol_rel = 1e-5
-# opt.xtol_abs = 0.001
-# opt.stopval = 0.01
-# opt.maxtime = 60.0
-opt.min_objective = test_initial_condition
-(minf, minx, ret) = optimize(opt, initial_x)
+function test_nlopt(;plot=false, prn=false, maxtime=60.0)
+    lower = SVector{2*SEGMENTS}(-10, -10, -20, -20, -10, -10.0, -5, -5, -5, -5, -5, -5)
+    upper = SVector{2*SEGMENTS}( 10,  10,  20,  20,  10,  10.0,  5,  5,  5,  5,  5,  5)
+    initial_x = MVector{2*SEGMENTS}(zeros(12))
+    init_392()
+    # working: :GD_STOGO 1803, :GD_STOGO_RAND 1403, :GN_ESCH 378, :GN_DIRECT_L 122, :GN_DIRECT_L_RAND 116, :GN_ISRES 82.8, :GN_CRS2_LM 60; not working: GN_AGS
+    # in 15 min :GN_CRS2_LM: 27;
+    # in  1 min :LN_SBPLX 9.96 in 15 min: 1.13
+    # in  1 min :LN_BOBYQA 0.31; LN_COBYLA 25; LD_MMA 6046; not working: LD_SLSQP
+    # without time limit: 0.0001699365589509266 ROUNDOFF_LIMITED; took 495s, 39628692 numevals or 12.5 us per numeval
+    opt = Opt(:LN_BOBYQA, 12) 
+    opt.lower_bounds = lower
+    opt.upper_bounds = upper
+    # opt.xtol_rel = 1e-5
+    # opt.xtol_abs = 0.001
+    # opt.stopval = 0.01
+    opt.maxtime = maxtime
+    println("\nStarted function test_nlopt... Will take $(opt.maxtime) seconds!")
+    opt.min_objective = test_initial_condition
+    (minf, minx, ret) = optimize(opt, initial_x)
+    if maxtime >= 60.0
+        show(@test minf < 0.18)
+    end
+    println("\nresult: $minx; minimum: $minf")
 
-my_state = KPS3.get_state()
-println("res2: "); display(my_state.res2)
-x = Float64[] 
-z = Float64[]
-for i in 1:length(my_state.pos)
-    push!(x, my_state.pos[i][1])
-    push!(z, my_state.pos[i][3])
-end  
-println(ret)
-println("minf: $minf")
+    my_state = KPS3.get_state()
+    if prn println("res2: "); display(my_state.res2) end
+    x = Float64[] 
+    z = Float64[]
+    for i in 1:length(my_state.pos)
+        push!(x, my_state.pos[i][1])
+        push!(z, my_state.pos[i][3])
+    end
+    if prn  
+        println(ret)
+        println("minf: $minf")
+    end
+    
+    if plot
+        lines(x, z)
+    end
+end
 
-lines(x, z)
+end
+using .TestNLopt 
