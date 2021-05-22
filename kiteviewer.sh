b1d3563ae4f2d0fb2b6a65bcacef26c1ac6d4068
Uwe Fechner <fechner@aenarete.eu>
2021-05-22 15:40:52 +0200

    Use branch dependant sys image.

--- kiteviewer.sh
+++ kiteviewer.sh
@@ -4,10 +4,11 @@ cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 if test -f "~/.bashrc"; then
     source ~/.bashrc
 fi
+branch=$(git rev-parse --abbrev-ref HEAD)
 
 echo "Lauching KiteViewer..."
-if test -f "MakieSys.so"; then
-    julia --startup-file=no  -J MakieSys.so --optimize=1 --project -e "include(\"./src/KiteViewer.jl\");main(true)"
+if test -f "MakieSys${branch}.so"; then
+    julia --startup-file=no  -J MakieSys${branch}.so --optimize=1 --project -e "include(\"./src/KiteViewer.jl\");main(true)"
 else
     julia --startup-file=no --optimize=1 --project -e "include(\"./src/KiteViewer.jl\");main(true)"
 fi
