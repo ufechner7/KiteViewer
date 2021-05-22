41a173b27b283de386c032d6cb8778edb9b8ff6c
Uwe Fechner <fechner@aenarete.eu>
2021-05-22 14:40:54 +0200

    Use a separate sys image per branch

--- create_sys_image.sh
+++ create_sys_image.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -eu
-if test -f "MakieSys.so"; then
-    mv MakieSys.so MakieSys.so.bak
+branch=$(git rev-parse --abbrev-ref HEAD)
+if test -f "MakieSys${branch}.so"; then
+    mv MakieSys${branch}.so MakieSys${branch}.so.bak
 fi
 rm -f Manifest.toml
 julia --project -e "include(\"./test/update_packages.jl\");"
 julia --project -e "using Pkg; Pkg.precompile()"
 julia --project -e "include(\"./test/create_sys_image.jl\");"
+mv MakieSys_tmp.so MakieSys${branch}.so
--- runjulia.sh
+++ runjulia.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-if test -f "MakieSys.so"; then
-    julia -J MakieSys.so --project -e "using Revise" -i
+branch=$(git rev-parse --abbrev-ref HEAD)
+if test -f "MakieSys${branch}.so"; then
+    julia -J MakieSys${branch}.so --project -e "using Revise" -i
 else
     julia --project -e "using Revise" -i
 fi
