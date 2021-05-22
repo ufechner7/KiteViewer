6e306f4b810e0d8295de7a70aa8961e5ccee83ab
Uwe Fechner <fechner@aenarete.eu>
2021-05-22 17:30:23 +0200

    Add the test folder to the module search path and use it

--- runjulia.sh
+++ runjulia.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 branch=$(git rev-parse --abbrev-ref HEAD)
 if test -f "MakieSys${branch}.so"; then
-    julia -J MakieSys${branch}.so --project -e "using Revise" -i
+    julia -J MakieSys${branch}.so --project -e "push!(LOAD_PATH,joinpath(pwd(),\"test\")); using Revise" -i
 else
     julia --project -e "using Revise" -i
 fi
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
+using TestOptim, TestNLopt
+
 include("test_kps3.jl")
 run_benchmarks()
-include("test_optim.jl")
 test_optim()
-include("test_nlopt.jl")
 test_nlopt()
