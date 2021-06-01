e33399802900fe34760b7e56f706a86a7a557224
Uwe Fechner <uwe.fechner@nl.abb.com>
2021-06-01 13:55:07 +0200

    Make sure all user modules get recompiled

--- create_sys_image.sh
+++ create_sys_image.sh
@@ -9,4 +9,7 @@ julia --project -e "using Pkg; Pkg.precompile()"
 julia --project -e "include(\"./test/create_sys_image.jl\");"
 mv MakieSys_tmp.so MakieSys${branch}.so
 julia --project -e "using Pkg; Pkg.precompile()"
+cd src
+touch *.jl # make sure all modules get recompiled in the next step
+cd ..
 julia --project -J MakieSys${branch}.so -e "push!(LOAD_PATH,joinpath(pwd(),\"src\"));using Utils, KCU_Sim, KPS3, Plot2D, RTSim"
