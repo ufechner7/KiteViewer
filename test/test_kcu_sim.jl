249614c0aa99409eab151dfbef32cea3bed2feb6
Uwe Fechner <fechner@aenarete.eu>
2021-06-13 21:31:41 +0200

    Fix warning

--- test/test_kcu_sim.jl
+++ test/test_kcu_sim.jl
@@ -1,6 +1,6 @@
-using Test, BenchmarkTools, StaticArrays, Revise, LinearAlgebra, SciMLBase
+using Test
 
-using KCU_Sim
+using KCU_Sim: calc_alpha_depower
 
 @testset "test_calc_alpha_depower" begin
     rel_depower = 0.25
@@ -8,7 +8,6 @@ using KCU_Sim
     @test 180.0 * alpha / π ≈ 2.1438208267482795
     rel_depower = 0.5
     alpha = calc_alpha_depower(rel_depower)
-    println(180.0 * alpha / π)
     @test 180.0 * alpha / π ≈ 39.56291863406212
 end
 
