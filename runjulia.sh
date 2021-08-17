eeaae5d8b754462432c707c341e3272b55b148d0
Uwe Fechner <fechner@aenarete.eu>
2021-08-17 22:49:59 +0200

    Add missing package search path for usage without image.

--- runjulia.sh
+++ runjulia.sh
@@ -3,5 +3,5 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 if test -f "MakieSys${branch}.so"; then
     julia -J MakieSys${branch}.so --project -e "push!(LOAD_PATH,joinpath(pwd(),\"test\"));push!(LOAD_PATH,joinpath(pwd(),\"src\")); using Revise" -i
 else
-    julia --project -e "using Revise" -i
+    julia --project -e "push!(LOAD_PATH,joinpath(pwd(),\"test\"));push!(LOAD_PATH,joinpath(pwd(),\"src\")); using Revise" -i
 fi
