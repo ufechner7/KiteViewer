a3c53a7ef8247b12b2bc42d54c9c47bf9c83697f
Uwe Fechner <fechner@aenarete.eu>
2021-11-13 17:30:21 +0100

    Fix bug #3

--- src/KiteViewer.jl
+++ src/KiteViewer.jl
@@ -21,7 +21,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. =#
 
 using GeometryBasics, Rotations, GLMakie, FileIO, LinearAlgebra, Printf
-AbstractPlotting.__init__()
 
 using Revise
 includet("./Utils.jl")
