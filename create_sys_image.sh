48eec7654e34c367278089ccf0590130e00bc956
Uwe Fechner <fechner@aenarete.eu>
2021-11-13 16:22:28 +0100

    Do not delete Manifest.toml

--- create_sys_image.sh
+++ create_sys_image.sh
@@ -3,7 +3,9 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 if test -f "MakieSys${branch}.so"; then
     mv MakieSys${branch}.so MakieSys${branch}.so.bak
 fi
-rm -f Manifest.toml
+if test -f "Manifest.toml"; then
+    mv Manifest.toml Manifest.toml.bak
+fi
 julia --project -e "include(\"./test/update_packages.jl\");"
 julia --project -e "using Pkg; Pkg.precompile()"
 julia --project -e "include(\"./test/create_sys_image.jl\");"
