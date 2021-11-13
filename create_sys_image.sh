690ad59dad5518d1e0894c09d4a7d6839debeeec
Uwe Fechner <fechner@aenarete.eu>
2021-11-13 19:20:58 +0100

    Improve robustness of image file creating, use default Manifest.toml, add parameter --update

--- create_sys_image.sh
+++ create_sys_image.sh
@@ -1,4 +1,17 @@
 #!/bin/bash -eu
+update=false
+if [[ $# -gt 0 ]]; then
+    if [[ $1 != "--update" ]]; then
+        echo "Invalid parameter! Use:"
+        echo "./create_sys_image.sh"
+        echo "or"
+        echo "./create_sys_image.sh --update"
+        exit 1
+    else
+        update=true
+    fi
+fi
+
 branch=$(git rev-parse --abbrev-ref HEAD)
 if [[ $branch == "main" ]]; then
     branch=""
@@ -6,10 +19,17 @@ fi
 if test -f "MakieSys${branch}.so"; then
     mv MakieSys${branch}.so MakieSys${branch}.so.bak
 fi
-if test -f "Manifest.toml"; then
-    mv Manifest.toml Manifest.toml.bak
+if [[ $update == true ]]; then
+    echo "Updating packages..."
+    if test -f "Manifest.toml"; then
+        mv Manifest.toml Manifest.toml.bak
+    fi
+    julia --project -e "include(\"./test/update_packages.jl\");"
+else
+    echo "Using default Manifest.toml ..."
+    cp Manifest.toml.default Manifest.toml
 fi
-julia --project -e "include(\"./test/update_packages.jl\");"
 julia --project -e "using Pkg; Pkg.precompile()"
+
 julia --project -e "include(\"./test/create_sys_image.jl\");"
 mv MakieSys_tmp.so MakieSys${branch}.so
