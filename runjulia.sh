66e538526f508a5ffe9123d4a2fe753d662cd3f5
Uwe Fechner <fechner@aenarete.eu>
2021-11-13 21:33:35 +0100

    Add error handler to runjulia.sh

--- runjulia.sh
+++ runjulia.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+RED='\033[1;31m'
+NC='\033[0m' # No Color
+
+function error_handler()
+{
+    printf "${RED}Error loading key packages!${NC}\n"
+    echo "Try to execute the commands:"
+    echo "    git checkout Project.toml"
+    echo "    ./create_sys_image.sh"
+    echo "first."
+}
+
+trap error_handler ERR
+
 branch=$(git rev-parse --abbrev-ref HEAD)
 if [[ $branch == "main" ]]; then
     branch=""
