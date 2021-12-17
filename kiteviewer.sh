7e287703098330059d8aae9ed80b611bc6561edf
Uwe Fechner <fechner@aenarete.eu>
2021-12-17 23:54:03 +0100

    Use jill to switch to Julia 1.6

--- kiteviewer.sh
+++ kiteviewer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
+jill switch 1.6
+
 if test -f "~/.bashrc"; then
     source ~/.bashrc
 fi
@@ -8,7 +10,7 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 
 echo "Lauching KiteViewer..."
 if test -f "MakieSys${branch}.so"; then
-    julia --startup-file=no  -J MakieSys${branch}.so --optimize=1 --project -e "push!(LOAD_PATH,joinpath(pwd(),\"src\"));include(\"./src/KiteViewer.jl\");main(true)"
+    julia --startup-file=no  -t auto -J MakieSys${branch}.so --optimize=1 --project -e "push!(LOAD_PATH,joinpath(pwd(),\"src\"));include(\"./src/KiteViewer.jl\");main(true)"
 else
-    julia --startup-file=no --optimize=2 --project -e "include(\"./src/KiteViewer.jl\");main(true)"
+    julia --startup-file=no -t auto --optimize=2 --project -e "include(\"./src/KiteViewer.jl\");main(true)"
 fi
