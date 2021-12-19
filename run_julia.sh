008436013cd087744c4029b17c71702e0a15956c
Uwe Fechner <fechner@aenarete.eu>
2021-12-19 03:37:37 +0100

    renamed file, added jill switch 1.6

--- run_julia.sh
+++ run_julia.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+jill switch 1.6
 branch=$(git rev-parse --abbrev-ref HEAD)
 if test -f "MakieSys${branch}.so"; then
     julia -J MakieSys${branch}.so --project -e "push!(LOAD_PATH,joinpath(pwd(),\"test\"));push!(LOAD_PATH,joinpath(pwd(),\"src\")); using Revise" -i
