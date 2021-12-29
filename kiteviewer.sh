dd125efce31f06f583ce889405aaa836a9c851c2
Uwe Fechner <fechner@aenarete.eu>
2021-12-30 00:21:15 +0100

    Improve stability

--- kiteviewer.sh
+++ kiteviewer.sh
@@ -7,7 +7,7 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 
 echo "Lauching KiteViewer..."
 if test -f "MakieSys-${julia_major}-${branch}.so"; then
-    julia --startup-file=no  -t auto -J MakieSys-${julia_major}-${branch}.so --optimize=1 --project -e "push!(LOAD_PATH,joinpath(pwd(),\"src\"));include(\"./src/KiteViewer.jl\");main(true)"
+    julia --startup-file=no  -t 1 -J MakieSys-${julia_major}-${branch}.so --optimize=2 --project -e "push!(LOAD_PATH,joinpath(pwd(),\"src\"));include(\"./src/KiteViewer.jl\");main(true)"
 else
     julia --startup-file=no -t auto --optimize=2 --project -e "include(\"./src/KiteViewer.jl\");main(true)"
 fi
