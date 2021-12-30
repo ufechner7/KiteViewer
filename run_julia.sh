a11a0767b0b59fb2aaf778afe0ec18d84fdcc10c
Uwe Fechner <fechner@aenarete.eu>
2021-12-30 19:40:15 +0100

    copy Manifest only when creating system image

--- run_julia.sh
+++ run_julia.sh
@@ -3,7 +3,7 @@
 julia_version=$(julia --version | awk '{print($3)}')
 julia_major=${julia_version:0:3} 
 branch=$(git rev-parse --abbrev-ref HEAD)
-cp -u Manifest-${julia_major}.toml.default Manifest.toml
+# cp -u Manifest-${julia_major}.toml.default Manifest.toml
 if test -f "MakieSys-${julia_major}-${branch}.so"; then
     julia -J  MakieSys-${julia_major}-${branch}.so -t 1 --optimize=2 --project -e "push!(LOAD_PATH,joinpath(pwd(),\"test\"));push!(LOAD_PATH,joinpath(pwd(),\"src\")); using Revise" -i
 else
