b7192c4ece305b639792409e6102e4ebcf84e2cc
Uwe Fechner <fechner@aenarete.eu>
2021-12-30 17:28:59 +0100

    Use only one thread...

--- run_julia.sh
+++ run_julia.sh
@@ -5,7 +5,7 @@ julia_major=${julia_version:0:3}
 branch=$(git rev-parse --abbrev-ref HEAD)
 cp -u Manifest-${julia_major}.toml.default Manifest.toml
 if test -f "MakieSys-${julia_major}-${branch}.so"; then
-    julia -J  MakieSys-${julia_major}-${branch}.so --project -e "push!(LOAD_PATH,joinpath(pwd(),\"test\"));push!(LOAD_PATH,joinpath(pwd(),\"src\")); using Revise" -i
+    julia -J  MakieSys-${julia_major}-${branch}.so -t 1 --optimize=2 --project -e "push!(LOAD_PATH,joinpath(pwd(),\"test\"));push!(LOAD_PATH,joinpath(pwd(),\"src\")); using Revise" -i
 else
     julia --project -e "push!(LOAD_PATH,joinpath(pwd(),\"test\"));push!(LOAD_PATH,joinpath(pwd(),\"src\")); using Revise" -i
 fi
