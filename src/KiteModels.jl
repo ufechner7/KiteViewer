c8d25631b3cbaf2269475196c8d5f248db170728
Uwe Fechner <fechner@aenarete.eu>
2022-01-01 17:22:43 +0100

    Works fine now.

--- src/KiteModels.jl
+++ src/KiteModels.jl
@@ -16,6 +16,7 @@ const SVec3    = SVector{3, SimFloat}
 
 @with_kw mutable struct KPS3{S, T}
     set::Settings = se()
+    kcu::KCU = KCU()
     calc_cl = Spline1D(se().alpha_cl, se().cl_list)
     calc_cd = Spline1D(se().alpha_cd, se().cd_list)    
     v_wind::T =           zeros(S, 3)    # wind vector at the height of the kite
@@ -24,11 +25,14 @@ const SVec3    = SVector{3, SimFloat}
     drag_force::T =       zeros(S, 3)
 end
 
-function KPS3(set::Settings, kcu::KCUState)
+function KPS3(kcu::KCU)
     s = KPS3{SimFloat, KVec3}()
-    s.set = set
-    s.v_wind =            [set.v_wind, 0, 0]    # wind vector at the height of the kite
-    s.v_wind_gnd =        [set.v_wind, 0, 0]    # wind vector at reference height
+    s.set = kcu.set
+    s.kcu = kcu
+    s.calc_cl = Spline1D(s.set.alpha_cl, s.set.cl_list)
+    s.calc_cd = Spline1D(s.set.alpha_cd, s.set.cd_list)       
+    s.v_wind =            [s.set.v_wind, 0, 0]    # wind vector at the height of the kite
+    s.v_wind_gnd =        [s.set.v_wind, 0, 0]    # wind vector at reference height
     return s
 end
 
