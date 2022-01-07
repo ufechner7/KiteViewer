2bd80e01d2c4b9001abb3e5fad45aa81f20d4ae4
Uwe Fechner <fechner@aenarete.eu>
2022-01-07 22:58:56 +0100

    Cleanup list of dependencies, add compat entries

--- test/create_sys_image.jl
+++ test/create_sys_image.jl
@@ -1,12 +1,12 @@
 @info "Loading packages ..."
-using GLMakie, GeometryBasics, Arrow, MeshIO, RecursiveArrayTools, Revise, Rotations, StaticArrays, StructArrays, YAML, Sundials, NLsolve
+using GLMakie, GeometryBasics, Arrow, MeshIO, Revise, Rotations, StaticArrays, StructArrays, YAML, Sundials, NLsolve
 using PackageCompiler
 
 @info "Creating sysimage ..."
 push!(LOAD_PATH,joinpath(pwd(),"src"))
 
 PackageCompiler.create_sysimage(
-    [:GLMakie, :GeometryBasics, :Arrow, :MeshIO, :RecursiveArrayTools, :Revise, :Rotations, :StaticArrays, :StructArrays, :YAML, :Sundials, :NLsolve];
+    [:GLMakie, :GeometryBasics, :Arrow, :MeshIO, :Revise, :Rotations, :StaticArrays, :StructArrays, :YAML, :Sundials, :NLsolve];
     sysimage_path="MakieSys_tmp.so",
     precompile_execution_file=joinpath("test", "test_for_precompile.jl")
 )
