f0048190b0366b7e259997c9550f0310886dd273
Uwe Fechner <fechner@aenarete.eu>
2022-01-07 18:39:55 +0100

    Fix bug

--- kiteviewer.sh
+++ kiteviewer.sh
@@ -9,5 +9,5 @@ echo "Lauching KiteViewer..."
 if test -f "MakieSys-${julia_major}-${branch}.so"; then
     julia --startup-file=no  -t auto -J MakieSys-${julia_major}-${branch}.so --optimize=1 --project -e "push!(LOAD_PATH,joinpath(pwd(),\"src\"));include(\"./src/KiteViewer.jl\");main(true)"
 else
-    julia --startup-file=no -t auto --optimize=2 --project -e "include(\"./src/KiteViewer.jl\");main(true)"
+    julia --startup-file=no -t auto --optimize=2 --project -e "push!(LOAD_PATH,joinpath(pwd(),\"src\"));include(\"./src/KiteViewer.jl\");main(true)"
 fi
